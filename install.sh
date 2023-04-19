4dc349ed124a512932c7b657217790ad27aad212
Yamasaki Shotaro <88176012+seipan@users.noreply.github.com>
2023-04-19 21:03:48 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 INSTALL_SHELL=https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh
 mkdir -p $HOME/bin && curl -sfL $INSTALL_SHELL| sh -s -- -b $HOME/bin
 echo ::add-path::$HOME/bin
-echo ::add-path::$(go env GOPATH)/bin # for Go projects
+#!echo ::add-path::$(go env GOPATH)/bin # for Go projects
