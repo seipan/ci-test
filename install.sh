d762987f3f4c477e495525a40e1ec0095cba8de3
Yamasaki Shotaro <88176012+seipan@users.noreply.github.com>
2023-04-20 03:26:33 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-INSTALL_SHELL=https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh
-mkdir -p $HOME/bin && curl -sfL $INSTALL_SHELL| sh -s -- -b $HOME/bin
-echo ::add-path::$HOME/bin
-echo ::add-path::$(go env GOPATH)/bin # for Go projects
-
